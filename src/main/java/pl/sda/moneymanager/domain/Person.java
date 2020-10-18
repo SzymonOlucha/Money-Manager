bd661576953fb985725a0840e1f7b133e4583a37
Szymek582 <szymon.olucha@gmail.com>
2020-10-18 12:37:54 +0200

    [change] changed enume source and add data.sql

--- src/main/java/pl/sda/moneymanager/domain/Person.java
+++ src/main/java/pl/sda/moneymanager/domain/Person.java
@@ -6,6 +6,8 @@ import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
@@ -15,5 +17,10 @@ import javax.persistence.Entity;
 public class Person extends BaseEntity {
     private String name;
     private String surname;
+    @Enumerated(EnumType.STRING) // odpowiada na zamieniem zapisu w bazie dadnych na stringi (domylśnie jest ordinal czyli int)
     private Sex sex;
+
+
 }
+//insert into Person ( sex, surname, name)
+//values('MALE','P','A');
---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into Person ( sex, surname, name)
+values('MALE','P','A');
+
+insert into income_source (source_name)
+values('sda');
+
+insert into income(income_value_in_cent, person_id, income_source_id)
+values(1000, 1, 1);
